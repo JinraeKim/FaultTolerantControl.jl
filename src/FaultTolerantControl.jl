eac44a6c0c65ac5184fd02bcffb289b2f3970b76
JinraeKim <kjl950403@gmail.com>
2021-06-12 18:49:26 +0900

    Add FDI and fault

--- src/FaultTolerantControl.jl
+++ src/FaultTolerantControl.jl
@@ -1,5 +1,20 @@
 module FaultTolerantControl
 
-# Write your package code here.
+# if you don't reexport `FlightSims`, many APIs, e.g., `State` would report an issue like:
+# ERROR: Define the structure of state for your environment
+using Reexport
+@reexport using FlightSims
+using LinearAlgebra
+using UnPack
+
+# Fault
+export AbstractFault
+export AbstractActuatorFault, LoE
+# FDI (fault detection and isoltion)
+export AbstractFDI, LPFFDI
+
+include("Fault.jl")
+include("FDI.jl")
+
 
 end
