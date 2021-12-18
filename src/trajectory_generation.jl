09af65071c82aecd9ace07832c85d41a2d7f86e5
Jinrae Kim <kjl950403@gmail.com>
2021-12-18 19:31:24 +0900

    Add Bezier struct (#55)

--- src/trajectory_generation.jl
+++ src/trajectory_generation.jl
@@ -1,13 +1,21 @@
+abstract type AbstractTrajectory end
+
 """
 Bezier curve
 θ: a vector whose length is (N+1); each element can also be vector,
 e.g., θ = [[1, 2, 3], [2, 3, 4]]
 """
-function Bezier(θ::AbstractVector; t0::Number=0.0, tf::Number=1.0)
-    N = length(θ)-1
-    function bezier(t)
-        s = (t-t0)/(tf-t0)
-        basis = 0:N |> Map(i -> binomial(N, i) * s^i * (1-s)^(N-i)) |> collect
-        basis .* θ |> sum  # sum of N_C_i * s^(N-i) * (1-s)^i * θ_i
-    end
+struct Bezier <: AbstractTrajectory
+    θ::AbstractVector
+    t0::Real
+    tf::Real
+end
+
+function (bezier::Bezier)(t::Real)
+    @unpack t0, tf, θ = bezier
+    N = length(θ) -1
+    @assert t <= tf && t >= t0
+    s = (t-t0)/(tf-t0)
+    basis = 0:N |> Map(i -> binomial(N, i) * s^i * (1-s)^(N-i)) |> collect
+    basis .* θ |> sum  # sum of N_C_i * s^(N-i) * (1-s)^i * θ_i
 end
