a75b819902e3615e47637601225c34587ac8363f
JinraeKim <kjl950403@gmail.com>
2022-01-05 19:28:18 +0900

    Add docs

--- src/costs.jl
+++ src/costs.jl
@@ -3,6 +3,11 @@ abstract type AbstractCostFunctional end
 
 """
 Default weight: 1 for position, (1/π^2) for angular velocity (1 m ≈ 180 deg)
+# Notes
+- Q_p: weight matrix for running cost (position)
+- Q_ω: weight matrix for running cost (angular velocity)
+- F_p: weight matrix for terminal cost (position)
+- F_ω: weight matrix for terminal cost (angular velocity)
 """
 struct PositionAngularVelocityCostFunctional <: AbstractCostFunctional
     Q_p::AbstractMatrix
