0a77170960e4a0469bdbb6e03e847fcccf41293c
Hanna Lee <dlgkssk707@gmail.com>
2022-02-02 21:24:11 +0900

    Add min_HSV

---
+++ test/gramian.jl
@@ -0,0 +1,26 @@
+using FaultTolerantControl
+using LinearAlgebra
+using Test
+
+
+@testset "gramian" begin
+    A = -Matrix(I, 4, 4)
+    B = [0.0 1.0 0.0 1.0]'
+    C = [0.0 0.0 1.0 1.0]
+
+    f(x, u, p, t) = A*x + B*u .+ p
+    g(x, u, p, t) = C*x
+
+    # System dimension
+    n = 4
+    m = 1
+    l = 1
+
+    x0 = [0.0, 0.0, 0.0, 0.0]
+    u0 = [0.0]
+
+    Wc = empirical_gramian(f, g, m, n, l; opt=:c, dt=0.01, tf=1.0, pr=zeros(4, 1), xs=x0, us=u0)
+    Wo = empirical_gramian(f, g, m, n, l; opt=:o, dt=0.01, tf=1.0, pr=zeros(4, 1), xs=x0, us=u0)
+	min_HSV = FaultTolerantControl.min_HSV(Wc, Wo)
+	@show Wc Wo min_HSV
+end
